403821768eb7c88ec7fde234aca2b0556f6bdc12
Frank <yoinspiration@outlook.com>
2023-10-29 05:57:59 +0800

    添加了db.json

---
+++ src/components/EventCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="event-card">
+    <h2>{{ event.title }}</h2>
+    <span>@{{ event.time }} on {{ event.date }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+h2 {
+  font-size: 20px;
+}
+
+.event-link {
+  color: #2c3e50;
+  text-decoration: none;
+}
+</style>
---
+++ src/views/EventListView.vue
@@ -0,0 +1,63 @@
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  name: 'EventListView',
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 123,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event.',
+          location: 'Meow Town',
+          date: 'January 28, 2022',
+          time: '12:00',
+          organizer: 'Kat Laydee'
+        },
+        {
+          id: 456,
+          category: 'food',
+          title: 'Community Gardening',
+          description: 'Join us as we tend to the community edible plants.',
+          location: 'Flora City',
+          date: 'March 14, 2022',
+          time: '10:00',
+          organizer: 'Fern Pollin'
+        },
+        {
+          id: 789,
+          category: 'sustainability',
+          title: 'Beach Cleanup',
+          description: 'Help pick up trash along the shore.',
+          location: 'Playa Del Carmen',
+          date: 'July 22, 2022',
+          time: '11:00',
+          organizer: 'Carey Wales'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 18px;
+}
+</style>
